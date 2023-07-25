eb25e93112d45f93b54f4d62c456ad8bc5f009dc
R@hul <rahulron02143@gmail.com>
2023-07-25 03:04:10 -0500

    view departments
    showing the all available departments in the database using this feature.

--- ems/management/views.py
+++ ems/management/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
-from .models import Employee
-
+from .models import Employee,Department,Timesheet,Salary
 def home_page(request):
     return render(request,'home_page.html')
 
@@ -9,4 +8,6 @@ def get_all_employees(request):
     employees = Employee.objects.all()
     return render(request, 'employees.html', {'employees': employees})
 
-def add_employee(request):
+def get_all_departments(request):
+    departments = Department.objects.all()
+    return render(request, 'departments.html', {'departments': departments})
