77a6793be978adada1dc852629237d985b0a0669
R@hul <rahulron02143@gmail.com>
2023-07-25 01:03:00 -0500

    Update models.py
    created the salary and time sheet table.

--- ems/management/models.py
+++ ems/management/models.py
@@ -15,7 +15,26 @@ class Employee(AbstractUser):
     email = models.EmailField()
     position = models.CharField(max_length=126)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_created=True)
+    updated = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.username)
+
+
+class Timesheet(models.Model):
+    timesheet_id = models.AutoField(primary_key=True)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    date = models.DateField()
+    hours_worked = models.DecimalField(default=0.0)
+    created = models.DateTimeField(auto_created=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+
+class Salary(models.Model):
+    salary_id = models.AutoField(primary_key=True)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    monthly_salary = models.DecimalField(max_digits=8, decimal_places=2)
+    created = models.DateTimeField(auto_created=True)
+    updated = models.DateTimeField(auto_now_add=True)
 
